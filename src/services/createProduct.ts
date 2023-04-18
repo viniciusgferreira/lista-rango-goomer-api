0717d78486685c3a06659df36461aeb5fb0ddeeb
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-04-18 14:42:33 -0300

    service to add product to db

---
+++ src/services/createProduct.ts
@@ -0,0 +1,17 @@
+import { sequelize } from '../models/Database.js';
+import { ProductInput } from '../models/Product.js';
+
+export async function createProduct(product: ProductInput) {
+  const saleScheduleArrayQuery = product.saleSchedule.join('\',\'');
+  const insertQuery = `INSERT INTO "Product" ("id","name","category","price","photo","isOnSale","salePrice","saleDescription","saleSchedule","restaurantId") VALUES (DEFAULT,'${product.name}','${product.category}','${product.price}','${product.photo}','${product.isOnSale}','${product.salePrice}','${product.saleDescription}',ARRAY ['${saleScheduleArrayQuery}'],'${product.restaurantId}') RETURNING "id","name","category","price","photo","isOnSale","salePrice","saleDescription","saleSchedule","restaurantId";`;
+
+  try {
+    return await sequelize.query(insertQuery);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+      return [error.message, 0];
+    }
+  }
+}
+

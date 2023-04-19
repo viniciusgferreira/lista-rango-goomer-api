c1cfc155df8c9fe323c91adac6e6ebdbd66d5cfe
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-04-19 15:24:56 -0300

    check if restaurant exists before add product

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -3,6 +3,7 @@ import { listAllProducts } from '../services/listAllProducts.js';
 import { createProduct } from '../services/createProduct.js';
 import { formatInputProduct } from '../utils/formatter.js';
 import { validateProduct } from '../utils/validator.js';
+import { listRestaurantById } from '../services/listRestaurantById.js';
 
 export async function listProductsController(req: Request, res: Response) {
   const allProducts = await listAllProducts();
@@ -12,7 +13,10 @@ export async function listProductsController(req: Request, res: Response) {
 export async function addProductController(req: Request, res: Response) {
   const productInput = formatInputProduct(req);
 
-  if (!validateProduct(productInput)) { res.status(400).json('invalid input fields'); }
+  if (!validateProduct(productInput)) { res.status(400).json('invalid input fields'); return; }
+
+  const restaurant = await listRestaurantById(productInput.RestaurantId);
+  if (!restaurant) { res.status(400).json('RestaurantId does not exist'); return; }
 
   const result = await createProduct(productInput);
   result?.[1] ? res.status(201).json(result[0]) : res.status(500).send(result?.[0]);

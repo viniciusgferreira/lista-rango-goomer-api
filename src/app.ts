ae9ae79993f2c1288e77a857de8b4a62314a727c
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-04-19 15:23:14 -0300

    load modules with correct schema

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { router } from './routes/router.js';
+import { loadModules } from './models/index.js';
 //import { Restaurant } from './models/Restaurant.js';
 
 export const app = express();
 app.use(express.json());
 app.use('/api/v1/', router);
+
+await loadModules();

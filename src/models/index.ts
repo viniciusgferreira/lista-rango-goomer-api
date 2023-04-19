5022c338c82624b329a7ef8edec07c3fcb69064b
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-04-19 15:23:34 -0300

    assossiation between entities

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,9 @@ import { Product } from './Product.js';
 import { Restaurant } from './Restaurant.js';
 
 
-export function loadModules() {
+export async function loadModules() {
   Restaurant.hasMany(Product);
   Product.belongsTo(Restaurant);
+  await Restaurant.sync({ alter: true });
+  await Product.sync({ alter: true });
 }

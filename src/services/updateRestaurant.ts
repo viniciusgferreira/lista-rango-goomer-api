28904c6fc76939c1a1bab3fdc3e89c76b116cfc7
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-04-20 14:37:31 -0300

    edit restaurant service

---
+++ src/services/updateRestaurant.ts
@@ -0,0 +1,16 @@
+import { sequelize } from '../models/Database.js';
+import { RestaurantInput } from '../models/Restaurant.js';
+
+export async function updateRestaurant(restaurant: RestaurantInput, id: number) {
+  const workingScheduleArrayQuery = restaurant.workingSchedule.join('\',\'');
+  const query = `UPDATE "Restaurant" SET name = '${restaurant.name}', adress = '${restaurant.adress}', photo = '${restaurant.photo}', "workingSchedule" = ARRAY ['${workingScheduleArrayQuery}'] WHERE id = ${id} RETURNING "id","name","adress","photo","workingSchedule";`;
+  console.log(query);
+  try {
+    return await sequelize.query(query);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+      return [error.message, 0];
+    }
+  }
+}
